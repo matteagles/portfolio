f8a34245d11aa381dbca6abf54480f094931eef0
mattacedev <matt.acedev@gmail.com>
2024-05-24 20:06:08 +0100

    removed education

--- app/page.js
+++ app/page.js
@@ -32,7 +32,6 @@ export default async function Home() {
       <Experience />
       <Skills />
       <Projects />
-      <Education />
       <Blog blogs={blogs} />
       <ContactSection />
     </>
