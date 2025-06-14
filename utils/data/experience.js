d82757870e071d97f67a0add374d4f3ca92a9008
Lawrence <code.matt.eagles@gmail.com>
2025-06-14 09:20:00 +0000

    added links to experience

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -3,18 +3,21 @@ export const experiences = [
     id: 1,
     title: 'FrontEnd Developer and Frontend Team Leader',
     company: "Activedge",
-    project: "Bank Settlement Application"
+    project: "Bank Settlement Application",
+    profile: "https://www.linkedin.com/company/activedgetechnologies/"
   },
   {
     id: 2,
     title: "Backend Software Developer",
     company: "PlatoHub",
-    project: "Stock management application"
+    project: "Stock management application",
+    profile: "https://www.linkedin.com/company/platohub/"
   },
   {
     id: 3,
     title: "Wordpress Developer",
     company: "Sabi Prints",
-    project: "Sabi Research"
+    project: "Sabi Research",
+    profile: "#"
   }
 ]
